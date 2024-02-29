ef606369e49c482b4a22e83e5da8215b3bce514c
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2024-02-29 13:48:32 -0300

    feat: add GameManager.cs

---
+++ Assets/_DreamHub/_Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+namespace DreamHub
+{
+    public sealed class GameManager : Singleton<GameManager>
+    {
+        public enum GameState { Menu = 0, Dreaming = 1, Hub = 2, Pause = 3, GameOver = 4, }
+        public GameState CurrentGameState { get; private set; }
+
+        public static void SetGameState(GameState state)
+        {
+            Instance.CurrentGameState = state;
+        }
+    }
+}
