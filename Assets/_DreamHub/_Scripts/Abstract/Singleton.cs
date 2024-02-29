13b8cc0bcd02144d5ea9dbd567581a7623f1b206
Kaio Menezes <73968667+kaiomenezes57@users.noreply.github.com>
2024-02-29 03:09:32 -0300

    feat: first commit with initial configurations

---
+++ Assets/_DreamHub/_Scripts/Abstract/Singleton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace AcerolaJam
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
